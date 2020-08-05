fe5aacf28a7be5665d9b9541ec118e86c13749bb
avermeulen <andre.vermeulen@gmail.com>
2020-08-05 16:40:26 +0200

    added port number configuration

--- src/main/java/net/webapp/App.java
+++ src/main/java/net/webapp/App.java
@@ -11,11 +11,23 @@ import java.util.List;
 import static spark.Spark.*;
 
 public class App {
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+
+
     public static void main(String[] args) {
 
         List<String> users = new ArrayList<>();
 
         staticFileLocation("/public");
+
+        port(getHerokuAssignedPort());
 
         get("/", (req, res) -> {
 
