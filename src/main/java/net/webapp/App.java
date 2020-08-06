b9f88ee151d8c563e56c6f434bebfe269f000b9a
avermeulen <andre.vermeulen@gmail.com>
2020-08-06 06:34:21 +0200

    added h2 memory support

--- src/main/java/net/webapp/App.java
+++ src/main/java/net/webapp/App.java
@@ -1,5 +1,8 @@
 package net.webapp;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,6 +12,81 @@ import java.util.Map;
 import java.util.List;
 
 import static spark.Spark.*;
+
+interface UserService {
+
+    @SqlUpdate("INSERT INTO greet(name) VALUES (?)")
+    void addUser(String username);
+
+    @SqlUpdate("select name from greet")
+    List<String> users();
+
+    int userCount();
+
+}
+
+class UserServiceWithJdbi implements UserService {
+
+    Handle handle;
+
+    public UserServiceWithJdbi(Handle handle) {
+        this.handle = handle;
+    }
+
+    private boolean userExists(String  userName) {
+        int count = handle.select("select count(*) from greet where name = ?", userName)
+                .mapTo(int.class)
+                .findOnly();
+
+        return count > 0;
+    }
+
+
+    @Override
+    public void addUser(String username) {
+        if (!userExists(username)) {
+            handle.execute("insert into greet (name) values (?)", username);
+        }
+    }
+
+    @Override
+    public List<String> users() {
+        List<String> names = handle.createQuery("select name from greet")
+                        .mapTo(String.class)
+                        .list();
+        return names;
+    }
+
+    @Override
+    public int userCount() {
+        int count = handle.select("select count(*) from greet")
+                        .mapTo(int.class)
+                        .findOnly();
+        return count;
+    }
+}
+
+class UserServiceUsingList implements UserService {
+
+    List<String> users = new ArrayList<>();
+
+    public void addUser(String username) {
+        if (!username.trim().equals("") &&
+                !users.contains(username)) {
+            users.add(username);
+        }
+    }
+
+    public List<String> users() {
+        return new ArrayList<>(users);
+    }
+
+    public int userCount() {
+        return users.size();
+    }
+
+}
+
 
 public class App {
 
@@ -23,7 +101,13 @@ public class App {
 
     public static void main(String[] args) {
 
-        List<String> users = new ArrayList<>();
+        Jdbi jdbi = Jdbi.create("jdbc:h2:mem:greetdb", "sa", "");
+
+        Handle handle = jdbi.open();
+
+        handle.execute("create table greet ( id integer identity, name varchar(50) )");
+
+        UserService userService = new UserServiceWithJdbi(handle);
 
         staticFileLocation("/public");
 
@@ -32,8 +116,8 @@ public class App {
         get("/", (req, res) -> {
 
             Map<String, Object> map = new HashMap<>();
-            map.put("users", users);
-            map.put("counter", users.size());
+            map.put("users", userService.users());
+            map.put("counter", userService.userCount());
 
             return new ModelAndView(map, "hello.handlebars");
         }, new HandlebarsTemplateEngine());
@@ -41,12 +125,7 @@ public class App {
         post("/greet", (req, res) -> {
 
             String username = req.queryParams("username");
-
-            if (!username.trim().equals("") &&
-                !users.contains(username)) {
-                users.add(username);
-            }
-
+            userService.addUser(username);
 
             res.redirect("/");
             return null;
